9bf5dfa67ca3762c42deb1bf3de93da233dfdffa
Deiveehan Nallazhagappan (LCL-C) <Deiveehan.Nallazhagappan@loblaw.ca>
2020-05-22 20:41:53 -0400

    init message

---
+++ src/main/java/com/test/gitapp/GitappApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitappApplication.class, args);
+    }
+
+}
